b47dc41e6046c58116cedd52174e60d300154003
oussamaza23 <oussama.zahdi41@gmail.com>
2023-10-27 09:17:46 -0700

    test v

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	while(argv[0][i] != '\0')
+	{
+		printf("%c", argv[0][i]);
+		i++;
+	}
+	printf("\n");
+}
